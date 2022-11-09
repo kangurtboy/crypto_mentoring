0086aa13e4b6f85415bb85aada3d2f409743dd57
o_ebrahim <kangurt.boy@gmail.com>
2022-11-09 14:52:43 +0500

    Добавил тестовые данные

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-  <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+  <div  v-if="loader" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
     <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
       <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -230,8 +230,41 @@
 </template>
 
 <script>
+
 require('@/assets/css/app.css');
 
+export default {
+	data() {
+		return {
+			loader: false,
+			tickets: [{
+				name: 'TEST1',
+				price: 1234.4325
+			},
+			{
+				name: 'TEST2',
+				price: 74875.4325
+			},
+			{
+				name: 'TEST3',
+				price: 232.4325
+			},
+			{
+				name: 'TEST4',
+				price: 8987.4325
+			},
+			{
+				name: 'TEST5',
+				price: 5555.4325
+			},
+			]
+		}
+	},
+
+	methods() {
+		
+	}
+}
 </script>
 
 <style scr="@/assets/css/app.css"> </style>
