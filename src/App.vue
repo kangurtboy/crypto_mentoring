e5dc82b93660a58686b424e9aca64a4698010763
o_ebrahim <kangurt.boy@gmail.com>
2022-12-01 10:03:19 +0500

    Добавил разметку для пагинации и филтрации

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,15 @@
         Добавить
       </button>
     </section>
-
+	<div class="flex gap-4 items-center justify-center">
+		<button
+			class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Назад</button>
+		<button
+			class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Вперед</button>
+		<input type="text"
+			placeholder="Поиск"
+			class="block w-full pr-10 border-gray-300 text-gray-900 focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md p-2 mb-1 w-24 h-8">
+	</div>
       <hr class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
 		<div class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
