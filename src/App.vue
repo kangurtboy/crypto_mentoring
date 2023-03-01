cbe4144deeca20c4509b6aaca00f87833498b9c4
o_ebrahim <kangurt.boy@gmail.com>
2023-03-01 17:28:05 +0500

    Форматировка документа

--- src/App.vue
+++ src/App.vue
@@ -1,163 +1,120 @@
 <template>
 	<div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-  <div  v-if="loader" class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
-    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
-  </div>
-  <div class="container">
-    <section>
-      <div class="flex">
-        <div class="max-w-xs">
-          <label for="wallet" class="block text-sm font-medium text-gray-700"
-            >Тикер</label
-          >
-          <div class="mt-1 relative rounded-md shadow-md">
-			<input 
-			v-model="userInput"
-			@keydown.enter="addTicker(userInput)"
-			@input="onInput"
-			type="text" 
-			name="wallet" id="wallet"
-			class="block w-full pr-10 border-gray-300 text-gray-900 focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md p-2 mb-1"
-			placeholder="Например DOGE" />
-          </div>
-          <div v-if="suggestedTickers.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap p-2">
-            <span 
-			v-for="(suggest , id) of suggestedTickers"
-			:key="id"
-			@click="addTicker(suggest)"
-			class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-              {{suggest}}
-            </span>
-          </div>
-          <div v-if="errorMessage" class="text-sm text-red-600">{{errorMessage}}</div>
-        </div>
-      </div>
-      <button
-		@click="addTicker(userInput)"
-        type="button"
-        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-      >
-        <!-- Heroicon name: solid/mail -->
-        <svg
-          class="-ml-0.5 mr-2 h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="#ffffff"
-        >
-          <path
-            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-          ></path>
-        </svg>
-        Добавить
-      </button>
-    </section>
-	<div class="flex gap-4 items-center justify-center">
-		<button
-			v-if="page > 1"
-			class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-			@click="paginationPrev"
-			>Назад</button>
-		<button
-			class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-			@click="paginationNext"
-			v-if="hasNextPage"
-			>Вперед</button>
-		<input type="text"
-			placeholder="Поиск"
-			class="block w-full pr-10 border-gray-300 text-gray-900 focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md p-2 mb-1 w-24 h-8"
-			v-model="searchValue"
-			>
-	</div>
-      <hr class="w-full border-t border-gray-600 my-4" />
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-		<div class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-		@click="selectTicker(ticker)"
-		:class="{
-			'border-4': currentTicker === ticker ,
-		}"
-			v-for="(ticker , id) of filteredTickers" :key="id">
-		<div class="px-4 py-5 sm:p-6 text-center" :class="{
-							'bg-red-100': !ticker.price}">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              {{ticker.name}} - USD
-            </dt>
-			<img v-if="tickerLoading && tickers[tickers.length - 1] === ticker" src="./assets/loader.gif" alt="" class="w-10 mx-auto">
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">
-              {{ticker.price}}
-            </dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-		<button 
-			@click.stop="removeTicker(ticker)"
-			class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg>Удалить
-          </button>
-        </div>
-      </dl>
-	<h1 v-if="!tickers.length" class="text-xl text-center">Нет активных тикеров</h1>
-      <hr class="w-full border-t border-gray-600 my-4" />
-    <section class="relative" v-if="currentTicker">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{currentTicker.name}} - USD
-      </h3>
-	<div class="flex items-end border-gray-600 border-b border-l h-64" ref="stripesContainer">
-		<div v-for="(stripe , id) of stripesPersentage" class="bg-purple-800 border w-10" 
-		:key="id" 
-		:style="{
-			'height': `${stripe}%`
-		}" ref="stripeItem">
+		<div v-if="loader"
+			class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+			<svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+				viewBox="0 0 24 24">
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+				<path class="opacity-75" fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+				</path>
+			</svg>
+		</div>
+		<div class="container">
+			<section>
+				<div class="flex">
+					<div class="max-w-xs">
+						<label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
+						<div class="mt-1 relative rounded-md shadow-md">
+							<input v-model="userInput" @keydown.enter="addTicker(userInput)" @input="onInput"
+								type="text" name="wallet" id="wallet"
+								class="block w-full pr-10 border-gray-300 text-gray-900 focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md p-2 mb-1"
+								placeholder="Например DOGE" />
+						</div>
+						<div v-if="suggestedTickers.length"
+							class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap p-2">
+							<span v-for="(suggest, id) of suggestedTickers" :key="id" @click="addTicker(suggest)"
+								class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+								{{ suggest }}
+							</span>
+						</div>
+						<div v-if="errorMessage" class="text-sm text-red-600">{{ errorMessage }}</div>
+					</div>
+				</div>
+				<button @click="addTicker(userInput)" type="button"
+					class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+					<!-- Heroicon name: solid/mail -->
+					<svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+						viewBox="0 0 24 24" fill="#ffffff">
+						<path
+							d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z">
+						</path>
+					</svg>
+					Добавить
+				</button>
+			</section>
+			<div class="flex gap-4 items-center justify-center">
+				<button v-if="page > 1"
+					class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+					@click="paginationPrev">Назад</button>
+				<button
+					class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+					@click="paginationNext" v-if="hasNextPage">Вперед</button>
+				<input type="text" placeholder="Поиск"
+					class="block w-full pr-10 border-gray-300 text-gray-900 focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md p-2 mb-1 w-24 h-8"
+					v-model="searchValue">
+			</div>
+			<hr class="w-full border-t border-gray-600 my-4" />
+			<dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+				<div class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+					@click="selectTicker(ticker)" :class="{
+						'border-4': currentTicker === ticker,
+					}" v-for="(ticker, id) of filteredTickers" :key="id">
+					<div class="px-4 py-5 sm:p-6 text-center" :class="{
+						'bg-red-100': !ticker.price
+					}">
+						<dt class="text-sm font-medium text-gray-500 truncate">
+							{{ ticker.name }} - USD
+						</dt>
+						<img v-if="tickerLoading && tickers[tickers.length - 1] === ticker" src="./assets/loader.gif"
+							alt="" class="w-10 mx-auto">
+						<dd class="mt-1 text-3xl font-semibold text-gray-900">
+							{{ ticker.price }}
+						</dd>
+					</div>
+					<div class="w-full border-t border-gray-200"></div>
+					<button @click.stop="removeTicker(ticker)"
+						class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
+						<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#718096"
+							aria-hidden="true">
+							<path fill-rule="evenodd"
+								d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+								clip-rule="evenodd"></path>
+						</svg>Удалить
+					</button>
+				</div>
+			</dl>
+			<h1 v-if="!tickers.length" class="text-xl text-center">Нет активных тикеров</h1>
+			<hr class="w-full border-t border-gray-600 my-4" />
+			<section class="relative" v-if="currentTicker">
+				<h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+					{{ currentTicker.name }} - USD
+				</h3>
+				<div class="flex items-end border-gray-600 border-b border-l h-64" ref="stripesContainer">
+					<div v-for="(stripe, id) of stripesPersentage" class="bg-purple-800 border w-10" :key="id" :style="{
+						'height': `${stripe}%`
+					}" ref="stripeItem">
+					</div>
+				</div>
+				<button type="button" class="absolute top-0 right-0" @click="reset">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+						xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
+						viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
+						<g>
+							<path
+								d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+								fill="#718096" data-original="#000000"></path>
+						</g>
+					</svg>
+				</button>
+			</section>
 		</div>
 	</div>
-      <button
-        type="button"
-        class="absolute top-0 right-0"
-		@click="reset">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.com/svgjs"
-          version="1.1"
-          width="30"
-          height="30"
-          x="0"
-          y="0"
-          viewBox="0 0 511.76 511.76"
-          style="enable-background:new 0 0 512 512"
-          xml:space="preserve"
-        >
-          <g>
-            <path
-              d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-              fill="#718096"
-              data-original="#000000"
-            ></path>
-          </g>
-        </svg>
-      </button>
-    </section>
-  </div>
-</div>
 </template>
 
 <script>
 import { requestTickets } from './api';
-import {getAllTickersName} from './api';
+import { getAllTickersName } from './api';
 export default {
 	data() {
 		return {
@@ -179,7 +136,7 @@ export default {
 		}
 	},
 
-	methods : {
+	methods: {
 		addTicker(data) {
 			//добавление тикеров
 			if (this.userInput || data) {
@@ -189,7 +146,7 @@ export default {
 						this.errorMessage = 'Такой тикер уже добавлен';
 					}
 				});
-				
+
 				if (!this.errorMessage) {
 					this.tickerLoading = true;
 					this.tickers.push({});
@@ -214,7 +171,7 @@ export default {
 
 		selectTicker(ticker) {
 			//выборка тикеров
-			if (this.currentTicker) {		
+			if (this.currentTicker) {
 				this.reset();
 			}
 			this.renderStripes();
@@ -228,8 +185,8 @@ export default {
 				if (this.currentTicker) {
 					requestTickets(this.currentTicker.name).then(
 						r => {
-							this.calculateStripe();	
-							if(this.stripesPersentage.length > this.maxStripeItems){
+							this.calculateStripe();
+							if (this.stripesPersentage.length > this.maxStripeItems) {
 								this.stripesPersentage.shift();
 							}
 							this.stripes.push(r.USD);
@@ -240,7 +197,7 @@ export default {
 						}
 					);
 				}
-			}, 5000);		
+			}, 5000);
 		},
 
 		onInput() {
@@ -290,7 +247,7 @@ export default {
 			}
 			settings[action]();
 		},
-		
+
 		paginationPrev() {
 			//Пагинация назад
 			this.page--;
@@ -301,24 +258,24 @@ export default {
 			this.page++;
 		},
 
-		urlLoad(){
+		urlLoad() {
 			// загрузка параметров url по умолчанию
-			const params  = new URLSearchParams(window.location.search);
-			if(parseInt (params.get('page'))){
-				
-				this.page = parseInt (params.get('page'));
+			const params = new URLSearchParams(window.location.search);
+			if (parseInt(params.get('page'))) {
+
+				this.page = parseInt(params.get('page'));
 			}
 			this.searchValue = params.get('filter');
 		},
 
-		urlFiltering(){
+		urlFiltering() {
 			//филтрация по URL адресса
 			const url = new URL(window.location.href);
 			const params = new URLSearchParams(url.search);
-			params.set('page' , this.page);	
-			params.set('filter' , this.searchValue);
+			params.set('page', this.page);
+			params.set('filter', this.searchValue);
 			url.search = params;
-			window.history.pushState({} , `${document.title} = page = ${this.page}` , url);
+			window.history.pushState({}, `${document.title} = page = ${this.page}`, url);
 		},
 
 
@@ -331,7 +288,7 @@ export default {
 		}
 
 	},
-	
+
 	created: function () {
 		getAllTickersName(this);
 		this.updateRenderetTickers();
@@ -343,12 +300,12 @@ export default {
 		filteredTickers() {
 			//филтрация тикеров
 			let tickers = [];
-			
+
 			if (this.tickers.length) {
 				tickers = this.tickers;
 			}
 
-			if(this.searchValue){
+			if (this.searchValue) {
 				//при ввыода текста в поля поиска
 				tickers = this.tickers.filter(item => item.name.startsWith(this.searchValue.toUpperCase()));
 			}
@@ -370,31 +327,31 @@ export default {
 		},
 
 	},
-	watch:{
+	watch: {
 
-		filteredTickers(){
+		filteredTickers() {
 			this.saveTickersLocal('set');
-			
-			if(this.filteredTickers.length === 0 && this.page > 1){
+
+			if (this.filteredTickers.length === 0 && this.page > 1) {
 				this.paginationPrev();
 			}
 		},
 
-		searchValue(){
+		searchValue() {
 			this.currentTicker = null;
-			if(!this.searchValue){
+			if (!this.searchValue) {
 				this.searchValue = '';
 			}
 			this.urlFiltering();
 		},
 
-		page(){
+		page() {
 			this.currentTicker = null;
 			this.urlFiltering();
 		}
 	},
-	mounted : function (){
-		window.addEventListener('resize' , this.calculateStripe);
+	mounted: function () {
+		window.addEventListener('resize', this.calculateStripe);
 	}
 }
 </script>
