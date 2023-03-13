485cce0fd9194413a2256b6f96f73d24674121ff
o_ebrahim <kangurt.boy@gmail.com>
2023-03-13 11:26:31 +0500

    Вырезал разметку отрисовку графика

--- src/App.vue
+++ src/App.vue
@@ -54,28 +54,7 @@
 			</dl>
 			<h1 v-if="!tickers.length" class="text-xl text-center">Нет активных тикеров</h1>
 			<hr class="w-full border-t border-gray-600 my-4" />
-			<section class="relative" v-if="currentTicker">
-				<h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-					{{ currentTicker.name }} - USD
-				</h3>
-				<div class="flex items-end border-gray-600 border-b border-l h-64" ref="stripesContainer">
-					<div v-for="(stripe, id) of stripesPersentage" class="bg-purple-800 border w-10" :key="id" :style="{
-						'height': `${stripe}%`
-					}" ref="stripeItem">
-					</div>
-				</div>
-				<button type="button" class="absolute top-0 right-0" @click="reset">
-					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-						xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
-						viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
-						<g>
-							<path
-								d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-								fill="#718096" data-original="#000000"></path>
-						</g>
-					</svg>
-				</button>
-			</section>
+			<stripes-window-vue :current-ticker="currentTicker" @close-window="openWindow()"/>
 		</div>
 	</div>
 </template>
@@ -83,6 +62,7 @@
 <script>
 import { requestTickets } from './api';
 import addTicker from './components/addTicker';
+import stripesWindowVue from './components/stripesWindow.vue';
 export default {
 	data() {
 		return {
@@ -90,9 +70,6 @@ export default {
 			tickerLoading: false,
 			errorMessage: '',
 			currentTicker: null,
-			stripes: [],
-			stripesPersentage: [],
-			stripeInterval: null,
 			tickers: [],
 			paginationCount: 6,
 			page: 1,
@@ -100,7 +77,7 @@ export default {
 			maxStripeItems: 0,
 		}
 	},
-	components: { addTicker },
+	components: { addTicker , stripesWindowVue },
 
 	methods: {
 		addTicker(data) {
@@ -139,36 +116,17 @@ export default {
 			if (this.currentTicker) {
 				this.reset();
 			}
-			this.renderStripes();
 			this.currentTicker = ticker;
 		},
-		async renderStripes() {
-			//отрисовка столбиков для показа графика тикера
-			this.stripesPersentage = [];
-			this.stripes = [];
-			this.stripeInterval = setInterval(() => {
-				if (this.currentTicker) {
-					requestTickets(this.currentTicker.name).then(
-						r => {
-							this.calculateStripe();
-							if (this.stripesPersentage.length > this.maxStripeItems) {
-								this.stripesPersentage.shift();
-							}
-							this.stripes.push(r.USD);
-							const min = Math.min(...this.stripes);
-							const minPersent = 50;
-							const persentage = Math.floor(((r.USD - min) * 100) / 100) + minPersent;
-							this.stripesPersentage.push(persentage);
-						}
-					);
-				}
-			}, 5000);
-		},
+
 
 		reset() {
 			//сброс выбраного тикера
-			clearInterval(this.stripeInterval);
 			this.currentTicker = null;
+		},
+
+		openWindow(openedWindow){
+			console.log(openedWindow);
 		},
 
 		updateRenderetTickers() {
@@ -230,14 +188,6 @@ export default {
 			url.search = params;
 			window.history.pushState({}, `${document.title} = page = ${this.page}`, url);
 		},
-
-		calculateStripe() {
-			const stripeContainer = this.$refs.stripesContainer;
-			const stripeItemWidth = 38;
-			if (stripeContainer && stripeContainer.childElementCount) {
-				this.maxStripeItems = Math.floor(stripeContainer.clientWidth / stripeItemWidth);
-			}
-		}
 
 	},
 
